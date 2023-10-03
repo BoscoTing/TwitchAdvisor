bbbb3446c0529995d54feb101484077259da3d14
Hao Ting <boscoting0522@gmail.com>
2023-10-03 17:45:20 +0800

    chore: create new dags for tracking more channels as default channels.
    Copy dags files and change the channel to midbeast, perkz_lol

---
+++ scheduled_dags/perkz_lol_dags/insert_stats_perkz_lol_dag.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+import os
+import sys
+sys.path.insert(0, os.getcwd())
+
+from features.viewers_reaction import ViewersReactionAnalyser
+
+analyser = ViewersReactionAnalyser("perkz_lol")
+one_minute_ago = datetime.now().utcnow() - timedelta(minutes=1)
+with DAG(
+    "insert_stats_perkz_lol_dag",
+    schedule="@once", 
+    start_date=one_minute_ago,
+    concurrency=1,
+    max_active_runs=1
+) as dag:
+    insert=PythonOperator(
+        task_id="insert_stats_perkz_lol",
+        python_callable=analyser.insert_historical_stats,
+        )
