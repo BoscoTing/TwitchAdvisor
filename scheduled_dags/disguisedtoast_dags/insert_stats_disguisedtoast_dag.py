100f9db8964c30e557353c504797a079adeba6c8
Hao Ting <boscoting0522@gmail.com>
2023-10-07 00:50:48 +0800

    fix: Modify detect_livestream.py to trigger insert_stats dags after channels became off-line.
    The modified version is in test_detect_livestream.py and will be copy to the ec2 instance where run the Airflow.
    modified the code in insert_stats_disguisedtoast_dag.py to work with changes in test_detect_livestream.py.

--- scheduled_dags/disguisedtoast_dags/insert_stats_disguisedtoast_dag.py
+++ scheduled_dags/disguisedtoast_dags/insert_stats_disguisedtoast_dag.py
@@ -1,5 +1,7 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+import airflow.settings
+from airflow.models import DagModel
 from datetime import datetime, timedelta
 import os
 import sys
@@ -7,16 +9,41 @@ sys.path.insert(0, os.getcwd())
 
 from features.viewers_reaction import ViewersReactionAnalyser
 
-analyser = ViewersReactionAnalyser("disguisedtoast")
+channel = "disguisedtoast"
+
+analyser = ViewersReactionAnalyser(channel)
 one_minute_ago = datetime.now().utcnow() - timedelta(minutes=1)
+
+def pause_dag(dag, is_paused):
+    session = airflow.settings.Session()
+    try:
+        qry = session.query(DagModel).filter(DagModel.dag_id == dag)
+        d = qry.first()
+        d.is_paused = is_paused
+        session.commit()
+    except:
+        session.rollback()
+    finally:
+        session.close()
+
 with DAG(
-    "insert_stats_disguisedtoast_dag",
+    f"insert_stats_{channel}_dag",
     schedule="@once",
     start_date=one_minute_ago,
     concurrency=1,
     max_active_runs=1
 ) as dag:
-    insert=PythonOperator(
-        task_id="insert_stats_disguisedtoast",
+    insert_stats=PythonOperator(
+        task_id=f"insert_stats_{channel}",
         python_callable=analyser.insert_historical_stats,
         )
+    pause=PythonOperator(
+        task_id=f"pause_insert_stats_{channel}",
+        python_callable=pause_dag,
+        op_kwargs={
+            "dag": f"insert_stats_{channel}",
+            "is_paused": True
+            }
+        )
+# when insert_stats done, set the dag is_paused = True to ensure it won't be triggered by detect_channel_dag again.
+insert_stats > pause
