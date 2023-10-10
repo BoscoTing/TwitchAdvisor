6e564226e7883592e8d166db152b0a3804508b10
Hao Ting <boscoting0522@gmail.com>
2023-10-10 19:22:40 +0800

    fix: Let the weekday on x-axis start from Monday on channel overview plot

--- features/channel_overview.py
+++ features/channel_overview.py
@@ -65,7 +65,7 @@ class Overview:
             doc['weekOfMonth'] = (doc['dayOfMonth'] - 1) // 7 + 1
             doc['weekOfYear'] = started_at_date.isocalendar().week
 
-            weekday_names = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+            weekday_names = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
             doc['weekDayName'] = weekday_names[doc['weekDay']]
 
             if week == doc['weekOfYear'] and year == doc['year']:
