0b13c3c1bf5fff713a6bf96514f7e5b422c9bd4a
Hao Ting <boscoting0522@gmail.com>
2023-10-15 21:26:45 +0800

    docs: organize the directories to be the same as develop branch

--- app/application.py
+++
@@ -1,8 +0,0 @@
-from server import app
-from waitress import serve
-
-if __name__ == "__main__":
-    if (app.env == "production"):
-        serve(app, host='0.0.0.0', port = 3000)
-    else:
-        app.run('0.0.0.0', port = 8000)
