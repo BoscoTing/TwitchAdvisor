085bc85063cf197a210cfb2733bb613a178be695
Hao Ting <boscoting0522@gmail.com>
2023-10-26 17:57:39 +0800

    chore: add stress test script

---
+++ App/server/tests/stress_test.js
@@ -0,0 +1,13 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+
+export default function () {
+    const res = http.get("https://twitch-advisor.com")
+    check(res, { 'status was 200': (r) => r.status === 200 });
+    sleep(1);
+}
+
+export const options = {
+    vus: 130,
+    duration: '30s',
+};
