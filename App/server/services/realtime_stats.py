420005366ee3af95c8201c2e27c0eaa87dbbc590
Hao Ting <boscoting0522@gmail.com>
2023-11-28 21:35:43 +0800

    chore: set chat_logs error as debug logging level

--- App/server/services/realtime_stats.py
+++ App/server/services/realtime_stats.py
@@ -48,7 +48,7 @@ class TwitchChatListenerTEMP():
             self.started_at = TwitchDeveloper().detect_living_channel(self.channel)['started_at'] # already turn timezone to +8 for showing on the chart.
 
         except Exception as e:
-            dev_logger.error(e)
+            dev_logger.debug(e)
 
             return False
     
